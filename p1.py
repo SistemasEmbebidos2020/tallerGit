c2e9e8a427b0c78e478ca7bf88553de0682c5be9
JeanVillamar <jfvillam@espol.edu.ec>
2025-09-15 21:04:09 -0500

    Preprocesamiento

--- p1.py
+++ p1.py
@@ -1,1 +1,13 @@
-print("Hola desde vscode")
+import pandas as pd
+
+
+
+def limpiar_datos(csv_file):
+    df = pd.read_csv(csv_file)
+    df = df.dropna()  # Eliminar filas con valores nulos
+    df = df.reset_index(drop=True)
+    print("DataFrame procesado:\n", df.head())
+    return df
+
+if __name__ == "__main__":
+    limpiar_datos("data.csv")
